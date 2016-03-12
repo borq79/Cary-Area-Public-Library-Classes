3de6da2f433373a98b51fa5a40156919859bda11
Ryan Brock <rbrock@zebra.com>
2016-03-12 17:01:05 -0600

    updated the LED array example so you can change the number of LEDs without changing the code in more than one spot

--- photoresistors/pcb_photoresistor_led_array_example/pcb_photoresistor_led_array_example.ino
+++ photoresistors/pcb_photoresistor_led_array_example/pcb_photoresistor_led_array_example.ino
@@ -12,84 +12,101 @@
 #define LED_ARRAY_ELEMENT_3 10
 #define LED_ARRAY_ELEMENT_4 11
 
+// Sample rate
+#define PHOTORESISTOR_SAMPLE_RATE_IN_MS 50
+
 static int ledArray[5] = {LED_ARRAY_ELEMENT_0, LED_ARRAY_ELEMENT_1, LED_ARRAY_ELEMENT_2, LED_ARRAY_ELEMENT_3, LED_ARRAY_ELEMENT_4};
-static const int ARRAY_SIZE = sizeof(ledArray) / sizeof(int); 
+static const int NUM_OF_LEDS_IN_ARRAY = sizeof(ledArray) / sizeof(int); 
+
+static const int LED_MIN_BRIGHTNESS = 0;
+static const int LED_MAX_BRIGHTNESS = 255;
+
+static const int PHOTORESISTOR_MIN_VALUE = 0;
+static const int PHOTORESISTOR_MAX_VALUE = 1023;
+
+static const int LED_RANGE = (LED_MAX_BRIGHTNESS - LED_MIN_BRIGHTNESS) + 1;
+static const int PHOTORESISTOR_RANGE = (PHOTORESISTOR_MAX_VALUE - PHOTORESISTOR_MIN_VALUE) + 1;
+
+static const int  PHOTORESISTOR_STEP = PHOTORESISTOR_RANGE / NUM_OF_LEDS_IN_ARRAY;
+static const float PHOTORESISTOR_TO_LED_MULTIPLIER = (float)LED_RANGE / (float)PHOTORESISTOR_STEP;
 
 void setup() {
   // Init the serial communications for debugging messages
   Serial.begin(9600);
   
+  Serial.print("Number of LEDs in the Array: "); Serial.println(NUM_OF_LEDS_IN_ARRAY);
+  Serial.print("LED Range: "); Serial.println(LED_RANGE);
+  Serial.print("Photoresistor Step: "); Serial.println(PHOTORESISTOR_STEP);
+  Serial.print("Photoresistor Range: "); Serial.println(PHOTORESISTOR_RANGE);
+  Serial.print("Multiplier: "); Serial.println(PHOTORESISTOR_TO_LED_MULTIPLIER);
+  
   // Iterate over the array and set the PWM pins to output
-  for(int i = 0; i < ARRAY_SIZE; i++) {
+  for(int i = 0; i < NUM_OF_LEDS_IN_ARRAY; i++) {
     pinMode(ledArray[i], OUTPUT);
   }
 }
 
 void loop() {
   // *** Uncomment this line if the data line runs from A0 on the PCB to A0 on the Arduino
-  int brightness = readAnalogSensorPin();
+  int photoresistorAnalogValue = readAnalogSensorPin();
   
-  updateLedArray(brightness);
+  updateLedArray(photoresistorAnalogValue);
   
   // Add this delay so the output is trottled to something reasonable
-  delay(50);
+  delay(PHOTORESISTOR_SAMPLE_RATE_IN_MS);
 }
 
 int readAnalogSensorPin() {
   // Read the value on the Analog pin - represents the value from the photoresistor PCB. Values are
   // 0 (bright) to 1023 (dark)
-  int analogVal = analogRead(SENSOR_ANALONG_PIN);
-  
-  Serial.println(analogVal); 
-  
-  return analogVal;
+  int photoresistorAnalogValue = analogRead(SENSOR_ANALONG_PIN);
+  return photoresistorAnalogValue;
 }
  
-void updateLedArray(int brightness) {
+void updateLedArray(int photoresistorAnalogValue) {
   // The PWM is 0-255, where 255 is 100% duty cycle and 0 is 0% duty cycle. The photoresistor is brightest when
-  // it is a analog value of 0, and darkest when it is 1023. There are more clever ways to do this than 
-  // a straight up compare of the ranges, but this is clearer. If you want, you can look into bit shifting and 
-  // masking to allow for dynamic array size without changing the algorithim, just the array size.
+  // it is a analog value of 0, and darkest when it is 1023. 
+  
+  // Invert the value to make the max the brightness value instead of 0 being the brightest
+  // Remember, a value of 0 (or min value set above) means no resistance and therefore means the room is bright
+  int brightness = PHOTORESISTOR_MAX_VALUE - photoresistorAnalogValue;
 
-  
-  // Brightness values 820 - 1023 light up LED 0 and the others are off
-  // Brightness values 615 - 819 light up LEDs 0 and 1 and the others are off
-  // Brightness values 410 - 614 light up LEDs 0-2 and the others are off
-  // Brightness values 205 - 409 light up LEDs 0-3 and the others are off
-  // Brightness values 0 - 204 light up LEDs 0-4
-  
-  int led0Brightness = 0;
-  int led1Brightness = 0;
-  int led2Brightness = 0;
-  int led3Brightness = 0;
-  int led4Brightness = 0;
-  
-  if (brightness >= 820 && brightness <= 1023) {
-    led0Brightness = (int)((float)brightness * 1.25);
-  } else if (brightness >= 615 && brightness <= 819) {
-    led0Brightness = 255;
-    led1Brightness = (int)((float)(brightness - 205) * 1.25);
-  } else if (brightness >= 410 && brightness <= 614) {
-    led0Brightness = led1Brightness = 255;
-    led2Brightness = (int)((float)(brightness - 410) * 1.25);
-  } else if (brightness >= 205 && brightness <= 409) {
-    led0Brightness = led1Brightness = led2Brightness = 255;
-    led3Brightness = (int)((float)(brightness - 615) * 1.25);
-  } else if (brightness <= 204) {
-    led0Brightness = led1Brightness = led2Brightness = led3Brightness = 255;
-    led4Brightness = (int)((float)(brightness - 820) * 1.25);
+  // This will take the 'floor' value of the brightness divided by the photoresistor step so that
+  // we know how many LEDs to just drive at full brightness. The reminder is used to determine
+  // the duty cycle of the last LED to drive.
+  int numberOfLedsToFullBrightness = brightness / PHOTORESISTOR_STEP;
+  int remainderOfPhotoresistorBrightnessForLastLED = brightness % PHOTORESISTOR_STEP;
+  int lastLEDPWMValue = (int)((float)remainderOfPhotoresistorBrightnessForLastLED * PHOTORESISTOR_TO_LED_MULTIPLIER);
+
+  if (photoresistorAnalogValue < PHOTORESISTOR_MIN_VALUE || photoresistorAnalogValue > PHOTORESISTOR_MAX_VALUE) {
+      Serial.print("Photoresistor value is out of expected range: "); Serial.println(photoresistorAnalogValue);
+      return;
   }
-  
 
-  Serial.println(led0Brightness);
-  Serial.println(led1Brightness);
-  Serial.println(led2Brightness);
-  Serial.println(led3Brightness);
-  Serial.println(led4Brightness);
-  
-  analogWrite(LED_ARRAY_ELEMENT_0, led0Brightness);
-  analogWrite(LED_ARRAY_ELEMENT_1, led1Brightness);
-  analogWrite(LED_ARRAY_ELEMENT_2, led2Brightness);
-  analogWrite(LED_ARRAY_ELEMENT_3, led3Brightness);
-  analogWrite(LED_ARRAY_ELEMENT_4, led4Brightness);
+  Serial.println(" ------------------------------------------------------------------- ");
+  Serial.print("Number of LEDs to Drive at 100% duty cycle: "); Serial.println(numberOfLedsToFullBrightness);
+  Serial.print("Remainder of last LED brightness: "); Serial.println(remainderOfPhotoresistorBrightnessForLastLED);
+  Serial.print("PWM value of the last LED in the array that is lit: "); Serial.println(lastLEDPWMValue);
+  Serial.print("Photoresitor analog reading: "); Serial.println(photoresistorAnalogValue);
+  Serial.print("Photoresitor brightness value: "); Serial.println(brightness);
+
+  int ledCounter = 0;
+
+  // Any LEDs that need to be driven at 100% duty cycle, do that now
+  for(ledCounter = 0; ledCounter < numberOfLedsToFullBrightness; ledCounter++) {
+      //Serial.print("LED "); Serial.print(ledCounter); Serial.println(" set to full brightness");
+      analogWrite(ledArray[ledCounter], LED_MAX_BRIGHTNESS);
+  }
+
+  if (ledCounter < NUM_OF_LEDS_IN_ARRAY) {
+      // Now set the duty cycle for the only LED that will be dynamic
+      //Serial.print("LED "); Serial.print(ledCounter); Serial.print(" is PWM value: "); + Serial.println(lastLEDPWMValue);
+      analogWrite(ledArray[ledCounter], lastLEDPWMValue);
+
+      // Turn off the remaining LEDs
+      for (ledCounter++; ledCounter < NUM_OF_LEDS_IN_ARRAY; ledCounter++) {
+          //Serial.print("LED "); Serial.print(ledCounter); Serial.println(" set to off");
+          analogWrite(ledArray[ledCounter], LED_MIN_BRIGHTNESS);
+      }
+  }
 }
